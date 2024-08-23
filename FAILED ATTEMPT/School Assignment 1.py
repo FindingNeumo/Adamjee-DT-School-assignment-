fc3d123c27b22369532a2a1e39f30cd4339f2eee
FindingNeumo <148970551+FindingNeumo@users.noreply.github.com>
2024-08-23 21:16:12 +0600

    Add files via upload

---
+++ FAILED ATTEMPT/School Assignment 1.py
@@ -0,0 +1,15 @@
+no1 = int(input("Enter 1st number: "))
+no2 = int(input("Enter 2nd number: "))
+no3 = int(input("Enter 3rd number: "))
+no4 = int(input("Enter 4th number: "))
+
+operation = input("Do you want to perform + (Addition) or x (Multiplication)? Enter '+' for Addition and 'x' for Multiplication: ").strip()
+
+if operation == '+':
+    result = no1 + no2 + no3 + no4
+    print('Addition =', result)
+elif operation == 'x':
+    result = no1 * no2 * no3 * no4
+    print('Multiplication =', result)
+else:
+    print("Invalid input! Please enter '+' for Addition or 'x' for Multiplication.")
