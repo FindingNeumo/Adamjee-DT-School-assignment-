0024f5dc67f653dcd60f1e42e986fdf042dfcdf9
FindingNeumo <148970551+FindingNeumo@users.noreply.github.com>
2024-08-23 21:19:12 +0600

    Update and rename School Assignment 1.py to School Assignment 1.py

--- Addition And Multiplication/School Assignment 1.py
+++ Addition And Multiplication/School Assignment 1.py
@@ -3,10 +3,12 @@ no2 = int(input("Enter 2nd number: "))
 no3 = int(input("Enter 3rd number: "))
 no4 = int(input("Enter 4th number: "))
 
-operation = input("Do you want to perform + (Addition) or x (Multiplication)? Enter '+' for Addition and 'x' for Multiplication: ").strip()
+operation = input("Enter '+' for Addition and 'x' for Multiplication: ").strip()
 
 if operation == '+':
     result = no1 + no2 + no3 + no4
+    print(▄▀█ █▀▄▀█ █░░ ▄▀█ █▄░█)
+    print(█▀█ █░▀░█ █▄▄ █▀█ █░▀█)
     print('Addition =', result)
 elif operation == 'x':
     result = no1 * no2 * no3 * no4
